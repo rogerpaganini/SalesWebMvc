3176fd4edc27451ee77731c7f41f53e707ce2a6f
Roger Paganini <rogerpaganini@hotmail.com>
2020-07-14 00:10:43 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web Mvc App from C# Course";
+            ViewData["Author"] = "Roger Alan Paganini";
 
             return View();
         }
